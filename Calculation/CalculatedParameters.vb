ffde3d8d9231cf60f20dcab9fdc10db439ec33bd
VictorMake <victormake393@gmail.com>
2020-10-07 10:01:55 +0300

    Добавьте файлы проекта.

---
+++ Calculation/CalculatedParameters.vb
@@ -0,0 +1,219 @@
+﻿Imports NationalInstruments.UI
+Imports NationalInstruments.UI.WindowsForms
+''' <summary>
+''' Расчетные параметры
+''' </summary>
+''' <remarks></remarks>
+Public Class CalculatedParameters
+    Implements IEnumerable
+    Public Property CalcDictionary As Dictionary(Of String, Parameter)
+
+    Default Public Property Item(key As String) As Double
+        Get
+            Return CalcDictionary(key).CalculatedValue
+        End Get
+        Set(value As Double)
+            CalcDictionary(key).CalculatedValue = value
+        End Set
+    End Property
+
+    'Public Function GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+    '    Return CalcDictionary.GetEnumerator()
+    'End Function
+
+    'Реализация интерфейса IEnumerable предполагает стандартную реализацию перечислителя.
+    ' Однако мы можем не полагаться на стандартную реализацию, а создать свою логику итератора с помощью ключевых слов Iterator и Yield.
+    ' Конструкция итератора представляет метод, в котором используется ключевое слово Yield для перебора по коллекции или массиву.
+    Public Iterator Function GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+        For Each keysCalc As String In CalcDictionary.Keys.ToArray
+            Yield CalcDictionary(keysCalc)
+        Next
+    End Function
+
+    Public Const G_СУМ_РАСХОД_ТОПЛИВА_КС_КП As String = "Gсум_расход_топливаКС_КП"
+    Public Const G_ОТБОРА_СУММАРНЫЙ As String = "Gрасход_отбора_сумм"
+    Public Const G_ОТБОРА_ОТНОСИТЕЛЬНЫЙ As String = "Gотбора_относительный"
+    Public Const G_ОТБОРА_ОТНОСИТЕЛЬНЫЙ_НАР As String = "Gотбора_относительный_нар"
+    Public Const G_ОТБОРА_ОТНОСИТЕЛЬНЫЙ_ВН As String = "Gотбора_относительный_вн"
+    Public Const АЛЬФА_КАМЕРЫ As String = "АльфаКамеры" ' суммарный коэф. избытка воздуха 
+    Public Const АЛЬФА_КП As String = "АльфаКП" ' коэффициент избытка воздуха К.П.
+    Public Const АЛЬФА_СУММАРНЫЙ As String = "АльфаСуммарный" ' коэффициент избытка воздуха суммарный
+    Public Const conЛЯМБДА As String = "Лямбда" ' приведенная скорость газового потока 
+    Public Const G_ВОЗДУХА As String = "Gвоздуха"
+    Public Const Т_СРЕДНЯЯ_ГАЗА_НА_ВХОДЕ As String = "Тсредняя_газа_на_входе" ' Средняя температура воздуха на входе
+    Public Const conT_ИНТЕГР As String = "T_интегр"
+    Public Const Т_Г_РАСЧЕТ As String = "Тг_расчет" ' расчетная температура газа
+    Public Const conКАЧЕСТВО As String = "Качество"
+    Public Const ПОЛОЖЕНИЕ_ТУРЕЛИ As String = "Положение_Турели"
+
+    Public Sub New()
+        CalcDictionary = New Dictionary(Of String, Parameter) From {
+        {G_СУМ_РАСХОД_ТОПЛИВА_КС_КП, New Parameter With {.Name = G_СУМ_РАСХОД_ТОПЛИВА_КС_КП}},
+        {G_ОТБОРА_СУММАРНЫЙ, New Parameter With {.Name = G_ОТБОРА_СУММАРНЫЙ}},
+        {G_ОТБОРА_ОТНОСИТЕЛЬНЫЙ, New Parameter With {.Name = G_ОТБОРА_ОТНОСИТЕЛЬНЫЙ}},
+        {G_ОТБОРА_ОТНОСИТЕЛЬНЫЙ_НАР, New Parameter With {.Name = G_ОТБОРА_ОТНОСИТЕЛЬНЫЙ_НАР}},
+        {G_ОТБОРА_ОТНОСИТЕЛЬНЫЙ_ВН, New Parameter With {.Name = G_ОТБОРА_ОТНОСИТЕЛЬНЫЙ_ВН}},
+        {АЛЬФА_КАМЕРЫ, New Parameter With {.Name = АЛЬФА_КАМЕРЫ}},
+        {АЛЬФА_КП, New Parameter With {.Name = АЛЬФА_КП}},
+        {АЛЬФА_СУММАРНЫЙ, New Parameter With {.Name = АЛЬФА_СУММАРНЫЙ}},
+        {conЛЯМБДА, New Parameter With {.Name = conЛЯМБДА}},
+        {G_ВОЗДУХА, New Parameter With {.Name = G_ВОЗДУХА}},
+        {Т_СРЕДНЯЯ_ГАЗА_НА_ВХОДЕ, New Parameter With {.Name = Т_СРЕДНЯЯ_ГАЗА_НА_ВХОДЕ}},
+        {conT_ИНТЕГР, New Parameter With {.Name = conT_ИНТЕГР}},
+        {Т_Г_РАСЧЕТ, New Parameter With {.Name = Т_Г_РАСЧЕТ}},
+        {conКАЧЕСТВО, New Parameter With {.Name = conКАЧЕСТВО}},
+        {ПОЛОЖЕНИЕ_ТУРЕЛИ, New Parameter With {.Name = ПОЛОЖЕНИЕ_ТУРЕЛИ}}}
+    End Sub
+
+    Public Sub BindingWithControls(key As String, inINumericPointer As INumericPointer, inNumericEdit As NumericEdit)
+        CalcDictionary(key).ControlNumericPointer = inINumericPointer
+        CalcDictionary(key).ControlNumericEdit = inNumericEdit
+    End Sub
+
+    Public Property Gсум_расход_топливаКС_КП_кг_час() As Double
+        Get
+            Return CalcDictionary(G_СУМ_РАСХОД_ТОПЛИВА_КС_КП).CalculatedValue
+        End Get
+        Set(ByVal value As Double)
+            CalcDictionary(G_СУМ_РАСХОД_ТОПЛИВА_КС_КП).CalculatedValue = value
+        End Set
+    End Property
+
+    Public Property Gрасход_отбора_сумм() As Double
+        Get
+            Return CalcDictionary(G_ОТБОРА_СУММАРНЫЙ).CalculatedValue
+        End Get
+        Set(ByVal value As Double)
+            CalcDictionary(G_ОТБОРА_СУММАРНЫЙ).CalculatedValue = value
+        End Set
+    End Property
+
+    Public Property Gотбора_относительный() As Double
+        Get
+            Return CalcDictionary(G_ОТБОРА_ОТНОСИТЕЛЬНЫЙ).CalculatedValue
+        End Get
+        Set(ByVal value As Double)
+            CalcDictionary(G_ОТБОРА_ОТНОСИТЕЛЬНЫЙ).CalculatedValue = value
+        End Set
+    End Property
+
+    Public Property Gотбора_относительный_нар() As Double
+        Get
+            Return CalcDictionary(G_ОТБОРА_ОТНОСИТЕЛЬНЫЙ_НАР).CalculatedValue
+        End Get
+        Set(ByVal value As Double)
+            CalcDictionary(G_ОТБОРА_ОТНОСИТЕЛЬНЫЙ_НАР).CalculatedValue = value
+        End Set
+    End Property
+
+    Public Property Gотбора_относительный_вн() As Double
+        Get
+            Return CalcDictionary(G_ОТБОРА_ОТНОСИТЕЛЬНЫЙ_ВН).CalculatedValue
+        End Get
+        Set(ByVal value As Double)
+            CalcDictionary(G_ОТБОРА_ОТНОСИТЕЛЬНЫЙ_ВН).CalculatedValue = value
+        End Set
+    End Property
+
+    Public Property АльфаКамеры() As Double
+        Get
+            Return CalcDictionary(АЛЬФА_КАМЕРЫ).CalculatedValue
+        End Get
+        Set(ByVal value As Double)
+            CalcDictionary(АЛЬФА_КАМЕРЫ).CalculatedValue = value
+        End Set
+    End Property
+
+    Public Property АльфаКП() As Double
+        Get
+            Return CalcDictionary(АЛЬФА_КП).CalculatedValue
+        End Get
+        Set(ByVal value As Double)
+            CalcDictionary(АЛЬФА_КП).CalculatedValue = value
+        End Set
+    End Property
+
+    Public Property АльфаСуммарный() As Double
+        Get
+            Return CalcDictionary(АЛЬФА_СУММАРНЫЙ).CalculatedValue
+        End Get
+        Set(ByVal value As Double)
+            CalcDictionary(АЛЬФА_СУММАРНЫЙ).CalculatedValue = value
+        End Set
+    End Property
+
+    Public Property Лямбда() As Double
+        Get
+            Return CalcDictionary(conЛЯМБДА).CalculatedValue
+        End Get
+        Set(ByVal value As Double)
+            CalcDictionary(conЛЯМБДА).CalculatedValue = value
+        End Set
+    End Property
+
+    Public Property Gвоздуха() As Double
+        Get
+            Return CalcDictionary(G_ВОЗДУХА).CalculatedValue
+        End Get
+        Set(ByVal value As Double)
+            CalcDictionary(G_ВОЗДУХА).CalculatedValue = value
+        End Set
+    End Property
+
+    Public Property ТсредняяГазаНаВходе() As Double
+        Get
+            Return CalcDictionary(Т_СРЕДНЯЯ_ГАЗА_НА_ВХОДЕ).CalculatedValue
+        End Get
+        Set(ByVal value As Double)
+            CalcDictionary(Т_СРЕДНЯЯ_ГАЗА_НА_ВХОДЕ).CalculatedValue = value
+        End Set
+    End Property
+
+    Public Property T_интегр() As Double
+        Get
+            Return CalcDictionary(conT_ИНТЕГР).CalculatedValue
+        End Get
+        Set(ByVal value As Double)
+            CalcDictionary(conT_ИНТЕГР).CalculatedValue = value
+        End Set
+    End Property
+
+    Public Property Тг_расчет() As Double
+        Get
+            Return CalcDictionary(Т_Г_РАСЧЕТ).CalculatedValue
+        End Get
+        Set(ByVal value As Double)
+            CalcDictionary(Т_Г_РАСЧЕТ).CalculatedValue = value
+        End Set
+    End Property
+
+    Public Property Качество() As Double
+        Get
+            Return CalcDictionary(conКАЧЕСТВО).CalculatedValue
+        End Get
+        Set(ByVal value As Double)
+            CalcDictionary(conКАЧЕСТВО).CalculatedValue = value
+        End Set
+    End Property
+
+    Public Property ПоложениеТурели() As Double
+        Get
+            Return CalcDictionary(ПОЛОЖЕНИЕ_ТУРЕЛИ).CalculatedValue
+        End Get
+        Set(ByVal value As Double)
+            CalcDictionary(ПОЛОЖЕНИЕ_ТУРЕЛИ).CalculatedValue = value
+        End Set
+    End Property
+End Class
+
+Public Class Parameter
+    'Public Enum TypeEnum
+    '    Pointer
+    '    NumericEdit
+    'End Enum
+    'Public Property Type As TypeEnum
+    Public Property Name As String
+    Public Property ControlNumericPointer As INumericPointer
+    Public Property ControlNumericEdit As NumericEdit
+    Public Property CalculatedValue As Double
+End Class
---
+++ Calculation/InputParameters.vb
@@ -0,0 +1,261 @@
+﻿''' <summary>
+''' Входные аргументы
+''' </summary>
+''' <remarks></remarks>
+Public Class InputParameters
+    Public Const conБАРОМЕТР As String = "Барометр"
+    Public Const conTБОКСА As String = "Tбокса" ' температура в боксе
+    Public Const T3_МЕРН_УЧАСТКА As String = "T3мерн_участка"
+    Public Const Т_ОТБОРА As String = "Тотбора"
+    Public Const Т_ОТБОРА_ВН As String = "ТотбораВн"
+    Public Const ДАВЛЕНИЕ_ВОЗДУХА_НА_ВХОДЕ As String = "ДавлениеВоздухаНаВходе"
+    Public Const ДАВЛЕНИЕ_МАГИСТРАЛЕ_ОТБОРА As String = "ДавлениеМагистралеОтбора"
+    Public Const ДАВЛЕНИЕ_МАГИСТРАЛЕ_ОТБОРА_ВН As String = "ДавлениеМагистралеОтбораВн"
+    Public Const ПЕРЕПАД_ДАВЛЕНИЯ_ВОЗДУХА_НА_ВХОДЕ As String = "ПерепадДавленияВоздухаНаВходе"
+    Public Const ПЕРЕПАД_ДАВЛЕНИЯ_ВОЗДУХА_ОТБОРА As String = "ПерепадДавленияВоздухаОтбора"
+    Public Const ПЕРЕПАД_ДАВЛЕНИЯ_ВОЗДУХА_ОТБОРА_ВН As String = "ПерепадДавленияВоздухаОтбораВн"
+    Public Const Р310_ПОЛНОЕ_ВОЗДУХА_НА_ВХОДЕ_КC As String = "Р310полное_воздуха_на_входе_КС" ' Полное Давление воздуха на входе в КС
+    Public Const Р311_СТАТИЧЕСКОЕ_ВОЗДУХА_НА_ВХОДЕ_КС As String = "Р311статическое_воздуха_на_входе_КС" ' Статическое давление на входе в  КС
+    Public Const Т_ТОПЛИВА_КС As String = "ТтопливаКС"
+    Public Const Т_ТОПЛИВА_КП As String = "ТтопливаКП"
+    Public Const Расход_Топлива_Камеры_Сгорания As String = "РасходТопливаКамерыСгорания"
+    Public Const РАСХОД_ТОПЛИВА_КАМЕРЫ_ПОДОГРЕВА As String = "РасходТопливаКамерыПодогрева"
+    Public Const ТВОЗДУХА_НА_ВХОДЕ_КП As String = "ТвоздухаНаВходеКП"
+
+    Private mБарометр As Double
+    Public Property Барометр() As Double
+        Get
+            Return mБарометр
+        End Get
+        Set(ByVal value As Double)
+            mБарометр = value
+            InputParameterDictionary(conБАРОМЕТР) = value
+        End Set
+    End Property
+
+    Private mTбокса As Double
+    Public Property Tбокса() As Double
+        Get
+            Return mTбокса
+        End Get
+        Set(ByVal value As Double)
+            mTбокса = value
+            InputParameterDictionary(conTБОКСА) = value
+        End Set
+    End Property
+
+    Private mT3мерн_участка As Double
+    Public Property T3мерн_участка() As Double
+        Get
+            Return mT3мерн_участка
+        End Get
+        Set(ByVal value As Double)
+            mT3мерн_участка = value
+            InputParameterDictionary(T3_МЕРН_УЧАСТКА) = value
+        End Set
+    End Property
+
+    Private mТотбора As Double
+    Public Property Тотбора() As Double
+        Get
+            Return mТотбора
+        End Get
+        Set(ByVal value As Double)
+            mТотбора = value
+            InputParameterDictionary(Т_ОТБОРА) = value
+        End Set
+    End Property
+
+    Private mТотбораВн As Double
+    Public Property ТотбораВн() As Double
+        Get
+            Return mТотбораВн
+        End Get
+        Set(ByVal value As Double)
+            mТотбораВн = value
+            InputParameterDictionary(Т_ОТБОРА_ВН) = value
+        End Set
+    End Property
+
+    Private mДавлениеВоздухаНаВходе As Double
+    Public Property ДавлениеВоздухаНаВходе() As Double
+        Get
+            Return mДавлениеВоздухаНаВходе
+        End Get
+        Set(ByVal value As Double)
+            mДавлениеВоздухаНаВходе = value
+            InputParameterDictionary(ДАВЛЕНИЕ_ВОЗДУХА_НА_ВХОДЕ) = value
+        End Set
+    End Property
+
+    Private mДавлениеМагистралеОтбора As Double
+    Public Property ДавлениеМагистралеОтбора() As Double
+        Get
+            Return mДавлениеМагистралеОтбора
+        End Get
+        Set(ByVal value As Double)
+            mДавлениеМагистралеОтбора = value
+            InputParameterDictionary(ДАВЛЕНИЕ_МАГИСТРАЛЕ_ОТБОРА) = value
+        End Set
+    End Property
+
+    Private mДавлениеМагистралеОтбораВн As Double
+    Public Property ДавлениеМагистралеОтбораВн() As Double
+        Get
+            Return mДавлениеМагистралеОтбораВн
+        End Get
+        Set(ByVal value As Double)
+            mДавлениеМагистралеОтбораВн = value
+            InputParameterDictionary(ДАВЛЕНИЕ_МАГИСТРАЛЕ_ОТБОРА_ВН) = value
+        End Set
+    End Property
+
+    Private mПерепадДавленияВоздухаНаВходе As Double
+    Public Property ПерепадДавленияВоздухаНаВходе() As Double
+        Get
+            Return mПерепадДавленияВоздухаНаВходе
+        End Get
+        Set(ByVal value As Double)
+            mПерепадДавленияВоздухаНаВходе = value
+            InputParameterDictionary(ПЕРЕПАД_ДАВЛЕНИЯ_ВОЗДУХА_НА_ВХОДЕ) = value
+        End Set
+    End Property
+
+    Private mПерепадДавленияВоздухаОтбора As Double
+    Public Property ПерепадДавленияВоздухаОтбора() As Double
+        Get
+            Return mПерепадДавленияВоздухаОтбора
+        End Get
+        Set(ByVal value As Double)
+            mПерепадДавленияВоздухаОтбора = value
+            InputParameterDictionary(ПЕРЕПАД_ДАВЛЕНИЯ_ВОЗДУХА_ОТБОРА) = value
+        End Set
+    End Property
+
+    Private mПерепадДавленияВоздухаОтбораВн As Double
+    Public Property ПерепадДавленияВоздухаОтбораВн() As Double
+        Get
+            Return mПерепадДавленияВоздухаОтбораВн
+        End Get
+        Set(ByVal value As Double)
+            mПерепадДавленияВоздухаОтбораВн = value
+            InputParameterDictionary(ПЕРЕПАД_ДАВЛЕНИЯ_ВОЗДУХА_ОТБОРА_ВН) = value
+        End Set
+    End Property
+
+    Private mР310полное_воздуха_на_входе_КС As Double
+    Public Property Р310полное_воздуха_на_входе_КС() As Double
+        Get
+            Return mР310полное_воздуха_на_входе_КС
+        End Get
+        Set(ByVal value As Double)
+            mР310полное_воздуха_на_входе_КС = value
+            InputParameterDictionary(Р310_ПОЛНОЕ_ВОЗДУХА_НА_ВХОДЕ_КC) = value
+        End Set
+    End Property
+
+    Private mР311статическое_воздуха_на_входе_КС As Double
+    Public Property Р311статическое_воздуха_на_входе_КС() As Double
+        Get
+            Return mР311статическое_воздуха_на_входе_КС
+        End Get
+        Set(ByVal value As Double)
+            mР311статическое_воздуха_на_входе_КС = value
+            InputParameterDictionary(Р311_СТАТИЧЕСКОЕ_ВОЗДУХА_НА_ВХОДЕ_КС) = value
+        End Set
+    End Property
+
+    Private mТтопливаКС As Double
+    Public Property ТтопливаКС() As Double
+        Get
+            Return mТтопливаКС
+        End Get
+        Set(ByVal value As Double)
+            mТтопливаКС = value
+            InputParameterDictionary(Т_ТОПЛИВА_КС) = value
+        End Set
+    End Property
+
+    Private mТтопливаКП As Double
+    Public Property ТтопливаКП() As Double
+        Get
+            Return mТтопливаКП
+        End Get
+        Set(ByVal value As Double)
+            mТтопливаКП = value
+            InputParameterDictionary(Т_ТОПЛИВА_КП) = value
+        End Set
+    End Property
+
+    Private mРасходТопливаКамерыСгорания As Double
+    Public Property РасходТопливаКамерыСгорания() As Double
+        Get
+            Return mРасходТопливаКамерыСгорания
+        End Get
+        Set(ByVal value As Double)
+            mРасходТопливаКамерыСгорания = value
+            InputParameterDictionary(Расход_Топлива_Камеры_Сгорания) = value
+        End Set
+    End Property
+
+    Private mРасходТопливаКамерыПодогрева As Double
+    Public Property РасходТопливаКамерыПодогрева() As Double
+        Get
+            Return mРасходТопливаКамерыПодогрева
+        End Get
+        Set(ByVal value As Double)
+            mРасходТопливаКамерыПодогрева = value
+            InputParameterDictionary(РАСХОД_ТОПЛИВА_КАМЕРЫ_ПОДОГРЕВА) = value
+        End Set
+    End Property
+
+    Private mТвоздухаНаВходеКП As Double
+    Public Property ТвоздухаНаВходеКП() As Double
+        Get
+            Return mТвоздухаНаВходеКП
+        End Get
+        Set(ByVal value As Double)
+            mТвоздухаНаВходеКП = value
+            InputParameterDictionary(РАСХОД_ТОПЛИВА_КАМЕРЫ_ПОДОГРЕВА) = value
+        End Set
+    End Property
+
+    'Public Const ОТСЕЧКА_ТУРЕЛИ As String = "Отсечка"
+    'Private mОтсечка As Double
+    'Public Property Отсечка() As Double
+    '    Get
+    '        Return mОтсечка
+    '    End Get
+    '    Set(ByVal value As Double)
+    '        mОтсечка = value
+    '        InputParameterDictionary(ОТСЕЧКА_ТУРЕЛИ) = value
+    '    End Set
+    'End Property
+
+    Public Property InputParameterDictionary As Dictionary(Of String, Double)
+
+    Public Sub New()
+        InputParameterDictionary = New Dictionary(Of String, Double) From {
+        {conБАРОМЕТР, Барометр},
+        {conTБОКСА, Tбокса},
+        {ДАВЛЕНИЕ_ВОЗДУХА_НА_ВХОДЕ, ДавлениеВоздухаНаВходе},
+        {ПЕРЕПАД_ДАВЛЕНИЯ_ВОЗДУХА_НА_ВХОДЕ, ПерепадДавленияВоздухаНаВходе},
+        {T3_МЕРН_УЧАСТКА, T3мерн_участка},
+        {ДАВЛЕНИЕ_МАГИСТРАЛЕ_ОТБОРА, ДавлениеМагистралеОтбора},
+        {ПЕРЕПАД_ДАВЛЕНИЯ_ВОЗДУХА_ОТБОРА, ПерепадДавленияВоздухаОтбора},
+        {Т_ОТБОРА, Тотбора},
+        {ДАВЛЕНИЕ_МАГИСТРАЛЕ_ОТБОРА_ВН, ДавлениеМагистралеОтбораВн},
+        {ПЕРЕПАД_ДАВЛЕНИЯ_ВОЗДУХА_ОТБОРА_ВН, ПерепадДавленияВоздухаОтбораВн},
+        {Т_ОТБОРА_ВН, ТотбораВн},
+        {Р310_ПОЛНОЕ_ВОЗДУХА_НА_ВХОДЕ_КC, Р310полное_воздуха_на_входе_КС},
+        {Р311_СТАТИЧЕСКОЕ_ВОЗДУХА_НА_ВХОДЕ_КС, Р311статическое_воздуха_на_входе_КС},
+        {Т_ТОПЛИВА_КС, ТтопливаКС},
+        {Т_ТОПЛИВА_КП, ТтопливаКП},
+        {Расход_Топлива_Камеры_Сгорания, РасходТопливаКамерыСгорания},
+        {РАСХОД_ТОПЛИВА_КАМЕРЫ_ПОДОГРЕВА, РасходТопливаКамерыПодогрева},
+        {ТВОЗДУХА_НА_ВХОДЕ_КП, ТвоздухаНаВходеКП}}
+
+        'InputParameterDictionary.Add(ОТСЕЧКА_ТУРЕЛИ, Отсечка)
+    End Sub
+
+End Class
